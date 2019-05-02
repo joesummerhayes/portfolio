00529c96be7c99daaa9d14319cd6aa598a84a6a4
Joe Summerhayes <joesummerhayes09@hotmail.com>
2019-05-02 10:17:08 +0100

    shadowing on projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 
 
@@ -6,17 +7,19 @@ class Project extends React.Component {
     render() {
         const {img, description, title} = this.props
         return (
-            <div className="col-12" style={{padding: "1rem"}}>
-            <div className="ui card">
-                <div className="image">
-                    <img src={img} alt={title} style={{maxHeight:"25rem"}}/>
+            <ProjectWrapper>
+                <div className="col-12" style={{padding: "1rem"}}>
+                    <div className="ui card expandCard">
+                        <div className="image" style={{maxHeight:"22rem"}}>
+                            <img src={img} alt={title} style={{height:"240.82px"}}/>
+                        </div>
+                        <div className="content" style={{padding: "1rem"}}>
+                            <span className="header">{title}</span>
+                            <div className="description">{description}</div>
+                        </div>
+                    </div>
                 </div>
-                <div className="content" style={{padding: "1rem"}}>
-                    <span className="header">{title}</span>
-                    <div className="description">{description}</div>
-                </div>
-            </div>
-        </div>
+            </ProjectWrapper>
         )
     }
 }
@@ -24,3 +27,14 @@ class Project extends React.Component {
 
 
 export default Project
+
+const ProjectWrapper = styled.div`
+    .expandCard {
+        transition: all 0.3s ease-in-out;
+    }
+
+    .expandCard:hover {
+        box-shadow: 7px 10px 5px 0px rgba(0,0,0,0.5);
+    transform: scale(1.2);
+    }
+`
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,28 +4,31 @@ import Project from './Project';
 import salahImg from '../images/salah.png';
 import premImg from '../images/prem.png';
 import teaAtOakimg from '../images/teaAtOak.png';
+import todolist from '../images/todolist.png';
+import recipe from '../images/recipe.png';
+import tech from '../images/tech.png';
 
 class ProjectList extends React.Component {
     render() {
         return (
-            <div className="container">
+            <div className="container py-5">
                 <div className="row">
-                    <div className="col-md-4">
-                    <Project title="prem app" img={premImg} description="change test"/>
+                    <div className="col-md-4 py-4">
+                    <Project title="prem app" img={premImg} description="he's our center half he's our number four"/>
                     </div>
                     <div className="col-md-4">
-                    <Project title="prem app" img={teaAtOakimg} description="hello from prem" />
+                    <Project title="prem app" img={teaAtOakimg} description="Anyone hungry?!" />
                     </div>
                     <div className="col-md-4">
-                    <Project title="prem app" img={premImg} description="hello from prem" />
+                    <Project title="to-do list" img={todolist} description="This was a todolist app I made YO" />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-4">
-                    <Project title="prem app" img={premImg} description="hello from prem" />
+                    <Project title="recipe app" img={recipe} description="what do I want to cook...." />
                     </div>
                     <div className="col-md-4">
-                    <Project title="prem app" img={premImg} description="hello from prem" />
+                    <Project title="tech app" img={tech} description="This is my tech app" />
                     </div>
                     <div className="col-md-4">
                     <Project title="prem app" img={premImg} description="hello from prem" />
