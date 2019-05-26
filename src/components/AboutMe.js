0e919e105d3b745a46e86b41f66f4bd2c11f6e9e
Joe Summerhayes <joesummerhayes09@hotmail.com>
2019-05-26 12:18:56 +0100

    content for about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -27,6 +27,26 @@ class AboutMe extends React.Component {
                 <div>
                 Javascript, CSS/ SASS, HMTL, React, Meteor, MongoDB, Redux, JQuery  
                 </div>
+                <h2 className="ui header text-capitalize">my story</h2>
+                In the Summer of 2018, after being inspired by a friend, I started to teach myself to
+code from scratch! I instantly fell in love with it and found myself coding at every given
+opportunity; on my lunch breaks at work, in the evenings, on the train. Being completely
+new to it all I had to start right from the beginning, learning basic HTML and CSS on
+Code Academy. I found Code Academy’s command line interface frustrating at times so moved to
+Udemy (which I’d heard great things about). I completed a fantastic extensive course on
+vanilla javascript that took me a couple of months to get through. After I had the basics down,
+I immediately looked to frameworks and decided Jquery would probably be a good stepping stone
+between vanilla JS and some of the larger frameworks. It was around this time I read an inspiring
+blog about an aspiring developer who decided to take on the ‘100 Day Coding Challenge’ - coding
+at least one hour, for a hundred days straight. Sounded like my cup of tea! I wanted to develop
+faster and this seemed like the perfect motivation! I started my first React course on day 1 and it’s
+safe to say the next 3 months was the best period of development, probably in my life. I’ve now
+completed two courses on React, made several great web apps, assisted the engineers at work
+with smaller sprint tickets and bugs, and started to dabble in my first experience of back end
+developing now! I’ve started a new, full-stack, course on Udemy, that focuses on creating a
+feedback management system. The plan for the next few months is to continue to learn more
+backend developing skills and to keep pushing myself in learning and practising React JS.
+
             </div>
             </>
         )
